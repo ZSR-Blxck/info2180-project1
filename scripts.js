57c4b5174ffd7f6991bbd84605b9cee695b7ce06
ZSR-Blxck <zavierrattray@gmail.com>
2021-10-24 06:19:30 -0500

    Added JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-/* Add your JavaScript to this file */
+window.onload=function()
+{
+	  document.querySelector("button").addEventListener("click", function(event){
+        event.preventDefault()
+        required();
+    });
+
+	function required() 
+    {
+    	var email= document.getElementById("email");
+    	message = ("Thank you! Your email address"+ email+ " has been added to our mailing list!");
+      // CHECKS IF FIELD IS EMPTY
+      if (email=="")
+      { 
+          Alert("Please enter a valid email address.");
+          return false;
+      }
+      	messageElement.innerHTML = message;
+          return true;
+    }
+
+}
